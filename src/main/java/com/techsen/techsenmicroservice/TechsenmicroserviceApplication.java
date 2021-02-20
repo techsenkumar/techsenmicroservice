f9effdebd667a0b741bc9de7b036c497c698d52a
senthil <techsen@gmail.com>
2021-02-20 16:50:21 +0530

    committing the soruce file

---
+++ src/main/java/com/techsen/techsenmicroservice/TechsenmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.techsen.techsenmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechsenmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechsenmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techsen/techsenmicroservice/TechsenmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsen.techsenmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechsenmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
